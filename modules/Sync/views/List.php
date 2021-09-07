91c3e167082581563c27a9231a96aa2d37edf602
Mr Rocky <jorgeadeviam1994@gmail.com>
2021-09-07 11:24:09 -0500

    test

--- modules/Sync/views/List.php
+++ modules/Sync/views/List.php
@@ -1,7 +1,6 @@
 <?php
-ini_set('display_errors','1'); 
-require_once('include/database/PearDatabase.php');
 require 'vendor/autoload.php';
+require_once('include/database/PearDatabase.php');
 
 use Salaros\Vtiger\VTWSCLib\WSClient;
 
@@ -11,7 +10,7 @@ class Sync_List_View extends Vtiger_Index_View {
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			switch ($_POST['form']) {
 				case "A":
-                    $client = new WSClient('http://192.168.99.102/_vtigercrm_2021/webservice.php', 'superadmin', 'MFaeyxCMTmRrUZiE');
+                    $client = new WSClient('http://192.168.99.102/_vtigercrm_2021/webservice.php', 'superadmin', '<MFaeyxCMTmRrUZiE>');
                     $var = $client->modules->getOne('Contacts'));
 					break;
 			}
