ddf7403f7c3cb1dae402c76739bf59a486679567
Mr Rocky <jorgeadeviam1994@gmail.com>
2021-09-07 14:02:37 -0500

    test

--- modules/Sync/views/List.php
+++ modules/Sync/views/List.php
@@ -31,8 +31,8 @@ class Sync_List_View extends Vtiger_Index_View {
                     }
                     $var = $Contact[id]
                     */
-                    $faker = Faker\Factory::create();
-                    $var = $faker->name;
+                    //$faker = Faker\Factory::create();
+                    $var = '$faker->name';
 					break;
 			}
 		} else {
